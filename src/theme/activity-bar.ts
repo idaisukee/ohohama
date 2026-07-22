5200558455ea98c2bb67a7f3f7adc47c7f7312a5
ieremius <hibariyashiki@gmail.com>
2026-07-22 17:06:54 +0900

    activity-bar

--- src/theme/activity-bar.ts
+++ src/theme/activity-bar.ts
@@ -6,7 +6,19 @@ export const activityBarColors = {
     "activityBar.foreground": semantic.active,
     "activityBar.inactiveForeground": semantic.inactive,
     "activityBarBadge.background": semantic.info,
-    
+
     "activityBarBadge.foreground": semantic.foreground,
     "activityBar.border": semantic.border,
+
+
+    "activityBar.activeBackground": semantic.background,
+    "activityBar.activeBorder": semantic.border,
+    "activityBar.activeFocusBorder": semantic.border,
+    "activityBar.dropBorder": semantic.border,
+    "activityBarTop.activeBackground": semantic.background,
+    "activityBarTop.activeBorder": semantic.border,
+    "activityBarTop.background": semantic.background,
+    "activityBarTop.dropBorder": semantic.border,
+    "activityBarTop.foreground": semantic.foreground,
+    "activityBarTop.inactiveForeground": semantic.inactive,
 } satisfies ThemeColors;
